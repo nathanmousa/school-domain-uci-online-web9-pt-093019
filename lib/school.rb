d9a5a026015d0c5c26c15c7b22fef2fd83a167ed
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-31 01:30:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,8 +18,8 @@ class School
   end
   
   def sort 
-    roster.each do |x, y| 
-      sorted[x] = y.sort 
+    roster.each do |grade, student| 
+      roster[grade] = student.sort 
     end 
   end
 end
